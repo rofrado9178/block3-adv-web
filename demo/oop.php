8aabb260d91284b0b3fb72994ae8c5e5fd8d4a8d
Fransiskus Aldo <rochesterfransiskusaldo@gmail.com>
2023-10-30 16:33:31 -0400

    adding oop php

--- demo/oop.php
+++ demo/oop.php
@@ -1,58 +1,58 @@
 <?php 
 
 class FannyPack{
-  public $color;
-  public $pocket;
-  public $price;
-  private $zipper;
+  private $color;
+  private $pocket;
+  private $price;
+  private $isZipperOpen = false;
 
-  function setColor($color){
-    $this-> color = $color;
-  }
-
-  function setPocket($pocket){
-    $this-> pocket = $pocket;
-  }
-
-  function setPrice($price){
-    $this-> price = $price;
-  }
-
-   function zipperAction($action){
-
-    if(!$action == "left"){
-      $this->zipper  = "close";
-      return;
-    }
-   return $this->zipper  = "open";
+  function __construct($color, $pocket, $price )
+  {
+    $this->color = $color;
+    $this->pocket = $pocket;
+    $this->price = $price;
 
   }
-
-
 
   function getDescription(){
-    echo $this->color," fanny pack <br>","color: ",$this->color,"<br>" ," pocket: ",$this->pocket, "<br>", "price: ", $this->price;
+    return "Fanny Pack color is ". $this->color. " with ". $this->pocket." pockets and the price is $ ". $this->price; 
   }
 
-  
+  function openZipper(){
+    $this->isZipperOpen = true;
+  }
 
-  
+   function closeZipper(){
+    $this->isZipperOpen = false;
+  }
+
+  function action($action){
+    if(!$this->isZipperOpen){
+        $this->openZipper();
+      }
+
+    if($action == "put" ||$action == "take"){
+      if($this->isZipperOpen == true){
+        echo "status of zipper is open <br>";
+      }
+      echo "Zipper is Open you can " , $action , " something <br>";
+      $this->closeZipper();
+       if($this->isZipperOpen == false){
+        echo "status of zipper is close";
+      }
+      return;
+    }
+    echo "you can only take something or put something";
+      return;
+  }
+
 }
 
-$blackFannyPack = new FannyPack();
-$blackFannyPack->setColor("black");
-$blackFannyPack->setPocket(3);
-$blackFannyPack->setPrice(40);
-$blackFannyPack->getDescription();
+$redFannyPack = new FannyPack("white", 3, 20);
+echo $redFannyPack->getDescription();
 
-
-echo "<br> <br>";
-
-$redFanny = new FannyPack();
-$redFanny->setColor("red");
-$redFanny->setPocket(5);
-$redFanny->setPrice(70);
-$redFanny->getDescription();
+echo "<br>";
+$redFannyPack->action("put");
 
 
 
